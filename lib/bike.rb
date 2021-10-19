9a27a3c0321edd83c104f1a4449417851d08a810
Matt Zimmer <zimmermja@gmail.com>
2021-10-19 17:07:45 +0100

    'working?' method in bike.rb updated to 'is_working?'; rspecs updated accordingly and passed; done to test functionality of predicate syntax

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,5 @@
 class Bike
-  def working?
+  def is_working?
     true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    expect(subject.release_bike).to be_working
+    expect(subject.release_bike).to be_is_working
   end
 end
 
