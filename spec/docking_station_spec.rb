f7a0ebee416c8f1bebd25542b9ec55f41c3601b4
Matt Zimmer <zimmermja@gmail.com>
2021-10-19 16:13:02 +0100

    rspec test refactored; original tests retained but commented out for future reference

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,13 @@
 require "docking_station"
 
 describe DockingStation do
-  it "Can respond to 'release_bike'" do
-    expect { DockingStation.new.release_bike }.not_to raise_error
-  end
+  #it "Can respond to 'release_bike'" do
+    # expect { DockingStation.new.release_bike }.not_to raise_error
+  #end
+
+  #it "Can respond to 'release_bike'" do
+    #expect(subject).to respond_to :release_bike
+  #end
+
+  it { is_expected.to respond_to :release_bike }
 end
