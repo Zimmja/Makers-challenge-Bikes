9253b7e01f27179b1c1efff984cacfa981fce523
Matt Zimmer <zimmermja@gmail.com>
2021-10-19 17:00:58 +0100

    docking_station.rb requires bike.rb; rspec test written and passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,18 @@
 require "docking_station"
 
 describe DockingStation do
-  #it "Can respond to 'release_bike'" do
-    # expect { DockingStation.new.release_bike }.not_to raise_error
-  #end
-
-  #it "Can respond to 'release_bike'" do
-    #expect(subject).to respond_to :release_bike
-  #end
-
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    expect(subject.release_bike).to be_working
+  end
 end
+
+=begin
+  it "Can respond to 'release_bike'" do
+    expect { DockingStation.new.release_bike }.not_to raise_error
+  end
+  it "Can respond to 'release_bike'" do
+    expect(subject).to respond_to :release_bike
+  end
+=end
